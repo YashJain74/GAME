02ac80c5a5ef1fee3686be545a2e62155f050f1c
YashJain74 <yashjain.sahab.2004@gmail.com>
2023-10-15 13:34:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let cardsarray= [
+    {   'name': 'IBM',
+         'img' : 'IBM.jpg',
+    },
+    {   'name': 'microsoft',
+         'img' : 'microsoft.jpg',
+    },
+    {   'name': 'FB',
+         'img' : 'fb.jpg',
+    },
+    {   'name': 'adobe',
+         'img' : 'adobe.jpg',
+    },
+    {   'name': 'amazon',
+         'img' : 'amazon.jpg',
+    },
+    {   'name': 'google',
+         'img' : 'google.jpg',
+    },
+    {   'name': 'tcs',
+         'img' : 'tcs.jpg',
+    }
+];
+const parentDiv=document.querySelector(selectors: '#cards');
